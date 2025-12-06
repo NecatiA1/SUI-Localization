23165546c724197db5b11a6f44c507554ba9d012
bilalbostan67 <bilal67bostan67@gmail.com>
2025-12-06 14:11:40 +0300

    deneme

--- src/app/transaction/page.js
+++
@@ -1,8 +0,0 @@
-export default function ScoreMapPage() {
-  return (
-    <main style={{ padding: "2rem" }}>
-      <h1>Score Map Page</h1>
-      <p>This is the transaction page.</p>
-    </main>
-  );
-}
